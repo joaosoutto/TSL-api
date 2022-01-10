d8d2da5836b32cc1c331a0f6a348d3a85d8d8ed2
joaosoutto <joaosoutto@hotmail.com>
2022-01-09 21:20:11 -0300

    adding posts to User

--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ['id', 'username', 'email', 'posts']
